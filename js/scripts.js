2aefb2f758b7270153024d6cbb25a31b9b1cb86a
mejia-dev <aaron.mejia8484@gmail.com>
2023-10-30 10:24:25 -0700

    Create objects and call addLocation to append destinations to list
    Co-authored-by: Nikkita Torres <nikkitatorres@yahoo.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,15 @@
+// Define TravelLocations
+
 function TravelLocations() {
-  this.destination = {};
+  this.destinations = {};
 }
 
+TravelLocations.prototype.addLocation = function(newDestination) {
+  this.destinations = newDestination;
+}
+
+
+// Define Destination
 function Destination(location, landmarks, timeOfYear, notes) {
   this.location = location,
   this.landmarks = landmarks,
@@ -9,5 +17,12 @@ function Destination(location, landmarks, timeOfYear, notes) {
   this.notes = notes;
 }
 
+// Create list and destinations
+
 let myTravelLocations = new TravelLocations();
 let portland = new Destination("Portland",["Welcome to Oregon Sign", "Rose Garden", "Lloyd Center Ice Rink"],"Fall","Home of Epicodus!");
+let seattle = new Destination("Seattle",["Space Needle", "Fish Market", "Gum Wall"],"Winter","ABC Gum is gross");
+let lasvegas = new Destination("Las Vegas",["MGM", "Caesar's Palace", "Eiffel Tower"],"Summer","Fake");
+myTravelLocations.addLocation(portland);
+myTravelLocations.addLocation(seattle);
+myTravelLocations.addLocation(lasvegas);
