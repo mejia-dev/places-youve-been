cd676180324d0a6a2911649e95c5f5813fc8565c
mejia-dev <aaron.mejia8484@gmail.com>
2023-10-30 11:18:00 -0700

    Add onload event listener and define displayInformation page
    Co-authored-by: Nikkita Torres <nikkitatorres@yahoo.com>

--- js/scripts.js
+++ js/scripts.js
@@ -20,9 +20,38 @@ function Destination(location, landmarks, timeOfYear, notes) {
 // Create list and destinations
 
 let myTravelLocations = new TravelLocations();
-let portland = new Destination("Portland",["Welcome to Oregon Sign", "Rose Garden", "Lloyd Center Ice Rink"],"Fall","Home of Epicodus!");
-let seattle = new Destination("Seattle",["Space Needle", "Fish Market", "Gum Wall"],"Winter","ABC Gum is gross");
-let lasvegas = new Destination("Las Vegas",["MGM", "Caesar's Palace", "Eiffel Tower"],"Summer","Fake");
+let portland = new Destination("Portland",["Welcome to Oregon Sign", " Rose Garden", " Lloyd Center Ice Rink"],"Fall","Home of Epicodus!");
+let seattle = new Destination("Seattle",["Space Needle", " Fish Market", " Gum Wall"],"Winter","ABC Gum is gross");
+let lasvegas = new Destination("Las Vegas",["MGM", " Caesar's Palace", " Eiffel Tower"],"Summer","Fake");
 myTravelLocations.addLocation(portland);
 myTravelLocations.addLocation(seattle);
 myTravelLocations.addLocation(lasvegas);
+
+
+// UI Logic
+function displayInformation() {
+  event.preventDefault();
+  const selection = document.getElementById("locationDropdown").value;
+  let resultsList = document.getElementById("listHolder");
+
+//Clear previous results
+resultsList.innerHTML = '';
+
+  let landmarksLi = document.createElement("li");
+  let timeOfYearLi = document.createElement("li");
+  let notesLi = document.createElement("li");
+
+
+  landmarksLi.append("Landmarks: " + myTravelLocations.destinations[selection].landmarks);
+  timeOfYearLi.append(" Time of Year Visited: " + myTravelLocations.destinations[selection].timeOfYear);
+  notesLi.append(" Notes: " + myTravelLocations.destinations[selection].notes);
+
+  
+  resultsList.appendChild(landmarksLi);
+  resultsList.appendChild(timeofYearLi);
+  resultsList.appendChild(notesLi);
+}
+
+window.addEventListener("load", function() {
+  this.document.getElementById("locationSelector").addEventListener("submit", displayInformation);
+})
